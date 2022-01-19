7468d60de830883bd1930d531920a70f5601324e
AkashSingh19 <68335126+AkashSingh19@users.noreply.github.com>
2022-01-19 19:10:26 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');
+const router = express.Router();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 const path = require('path');
 const cors = require('cors');
 // Cors 
@@ -10,13 +11,7 @@ const corsOptions = {
   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 }
 
-// Default configuration looks like
-// {
-//     "origin": "*",
-//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     "preflightContinue": false,
-//     "optionsSuccessStatus": 204
-//   }
+
 app.use(cors(corsOptions))
 app.use(express.static('public'));
 
@@ -29,9 +24,8 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 // Routes 
-app.use('/api/files', require('./routes/files'));
-app.use('/files', require('./routes/show'));
-app.use('/files/download', require('./routes/download'));
-
+app.use('api/files', require('./routes/files'));
+app.use('files', require('./routes/show'));
+app.use('files/download', require('./routes/download'));
 
 app.listen(PORT, console.log(`Listening on port ${PORT}.`));
